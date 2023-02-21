7e24b064643105d5520e9c8e324338f0441211bd
Nikolay <redecv8@gmail.com>
2023-02-22 01:20:48 +0300

    Widget and status event layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { TestPage } from "./pages/TestPage/TestPage";
 import { MainPage } from "./pages/MainPage/MainPage";
+import { GroupWidget } from "./common/Widget/GroupWidget";
 
 function App() {
-  return (
-    <Routes>
-      <Route element={<MainPage />} path="/"></Route>
-      <Route element={<TestPage />} path="/redux-test"></Route>
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route element={<MainPage />} path="/"></Route>
+            <Route element={<TestPage />} path="/redux-test"></Route>
+            <Route element={<GroupWidget />} path='/widget'></Route>
+        </Routes>
+    );
 }
 
 export default App;
