34c34cea8db24b3401e85b6e027650a1f6459ad3
Rain <silverain1qw@gmail.com>
2023-02-22 21:34:37 +0300

    UIKit1 buttonGroup refactoring

--- src/components/buttonGroup/btnGroupSelect.tsx
+++ src/components/buttonGroup/btnGroupSelect.tsx
@@ -1,36 +1,57 @@
-import React, {FC} from 'react';
-import {RadioBtnGroup} from './radioBtnGroup/radioBtnGroup';
-import {SelectBtnGroup} from './selectBtnGroup/selectBtnGroup';
-import {ButtonGroup, ButtonToolbar} from 'reactstrap';
+import React, { FC, useState } from 'react';
+import { Button, ButtonGroup, ButtonToolbar } from 'reactstrap';
+import styles from './buttonGroup.module.css';
 
-type btnGroup = { name: string; }[];
+type btnGroup = { name: string }[];
 
 interface BtnGroupI {
     view: string;
     data: btnGroup;
 }
 
+export const BtnGroupSelect: FC<BtnGroupI> = ({ view, data }) => {
+    const [rSelected, setRSelected] = useState<number | null>(0);
+    const [cSelected, setCSelected] = useState<number[]>([]);
 
-export const BtnGroupSelect: FC<BtnGroupI> = ({view, data}) => {
+    const handleRadioClick = (index: number) => setRSelected(index);
+
+    const handleSelectClick = (index: number) => {
+        const selectedIndex = cSelected.indexOf(index);
+        const newSelected = [...cSelected];
+        if (selectedIndex < 0) {
+            newSelected.push(index);
+        } else {
+            newSelected.splice(selectedIndex, 1);
+        }
+        setCSelected(newSelected);
+    };
+
+    const renderButton = (el: { name: string }, index: number) => {
+        const isActive =
+            view === 'radio' ? rSelected === index : cSelected.includes(index);
+
+        const handleClick =
+            view === 'radio' ? () => handleRadioClick(index) : () => handleSelectClick(index);
+
+        return (
+            <div key={index}>
+                <Button
+                    className={`${styles.btnItem} ${isActive ? styles.active : styles.deactive}`}
+                    color={isActive ? 'warning' : undefined}
+                    onClick={handleClick}
+                    active={isActive}
+                >
+                    {el.name}
+                </Button>
+            </div>
+        );
+    };
 
     return (
-        <div>
-            {view === 'radio' ? (
-                <ButtonToolbar>
-                    <ButtonGroup>
-                        <RadioBtnGroup data={data}/>
-                    </ButtonGroup>
-                </ButtonToolbar>
-            ) : view === 'select' &&
-                (
-                    <ButtonToolbar>
-                        <ButtonGroup>
-                            <SelectBtnGroup data={data}/>
-                        </ButtonGroup>
-                    </ButtonToolbar>
-                )
-            }
-        </div>
+        <ButtonToolbar>
+            <ButtonGroup>
+                <div className={styles.buttonsWrapper}>{data && data.map(renderButton)}</div>
+            </ButtonGroup>
+        </ButtonToolbar>
     );
-
-}
+};
