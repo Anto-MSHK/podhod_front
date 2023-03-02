b53477e05ba11c05aca5d26cb1220b57a4ef3f3f
Nikolay <redecv8@gmail.com>
2023-03-02 19:42:57 +0300

    Switch by event type

--- src/pages/EventOverviewPage/EventOverviewPage.tsx
+++ src/pages/EventOverviewPage/EventOverviewPage.tsx
@@ -1,4 +1,4 @@
-import React  from 'react'
+import React from 'react';
 import { StyledCard } from "../../components/StyledCard/StyledCard";
 import { BtnGroupSelect } from '../../components/ButtonGroup/ButtonGroup';
 import BtnFilters from '../../components/Filters/BtnFilters';
@@ -6,42 +6,95 @@ import styles from "./EventOverviewPage.module.css";
 import event1 from "../../assets/pictures/Event1.png";
 import event2 from "../../assets/pictures/Event2.png";
 import event3 from "../../assets/pictures/Event3.png";
+import { useState } from 'react';
+import { ButtonArt } from './../../components/ButtonArt/ButtonArt';
 
-const items =[
-    {name: "Fff", date: "12.06.2023", image: event1},
-    {name: "Dff", date: "12.06.2023", image: event2},
-    {name: "Tff", date: "12.06.2023", image: event3},
-] 
+interface Iitems {
+    id: number;
+    name: string;
+    date: string;
+    image: string;
+    type: string;
+    status: string;
+}
 
-
+const items = [
+    { id: 1, name: "1", date: "12.06.2023", image: event1, type: 'Выставка', status: 'draft' },
+    { id: 2, name: "2", date: "12.06.2023", image: event2, type: 'Выставка', status: 'completed' },
+    { id: 3, name: "3", date: "12.06.2023", image: event3, type: 'Выставка', status: 'published' },
+];
 
 const btnData = [
     { name: "Все" },
     { name: "Активные" },
     { name: "Просмотренные" },
     { name: "Черновик" },
-]
-
+];
 
 const sort = [{ name: "По дате" }, { name: "По типу" }];
 
 export function EventOverviewPage() {
+    const [item, setItems] = useState<Iitems[]>([]);
+
+    const handleSort = (status: string) => {
+        const draftCard = [...items];
+
+        switch (status) {
+            case 'draft':
+                setItems(draftCard.reduce((newArr: Iitems[], items) => {
+                    if (items.status === 'draft') {
+                        newArr.push(items);
+                    }
+                    return newArr;
+                }, []));
+                break;
+            case 'completed':
+                setItems(draftCard.reduce((newArr: Iitems[], items) => {
+                    if (items.status === 'completed') {
+                        newArr.push(items);
+                    }
+                    return newArr;
+                }, []));
+                break;
+            case 'published':
+                setItems(draftCard.reduce((newArr: Iitems[], items) => {
+                    if (items.status === 'published') {
+                        newArr.push(items);
+                    }
+                    return newArr;
+                }, []));
+                break;
+            default:
+                setItems(draftCard);
+        }
+    };
+
     return (
-     
+
         <div className={styles.wrapper}>
             <div><h1>Мероприятия</h1></div>
-
             <div className={styles.filter}>
                 <BtnGroupSelect view="radio" data={btnData} />
                 <div style={{ margin: "10px 0" }} />
                 <BtnFilters sort={sort} date={"12.02.2023"} type={""} />
             </div>
-           <div className={styles.card}>
-         
-                {items.map((item)=>(<StyledCard eventTitle={item.name} dateOfCreation={item.date} image={item.image}/>))}
-           </div>
-            
-
+            <div className={styles.card}>
+                {item.map((item) => (
+                    <StyledCard
+                        eventTitle={item.name}
+                        dateOfCreation={item.date}
+                        image={item.image}
+                        type={item.type}
+                        status={item.status}
+                        key={item.id} />
+                ))}
+            </div>
+            <div style={{ display: 'flex' }}>
+                <ButtonArt onClick={() => handleSort('draft')}>draft</ButtonArt>
+                <ButtonArt onClick={() => handleSort('completed')}>completed</ButtonArt>
+                <ButtonArt onClick={() => handleSort('published')}>published</ButtonArt>
+                <ButtonArt onClick={() => handleSort('')}>ALL</ButtonArt>
+            </div>
 
         </div>
     );
--- src/pages/StylePage/StylePage.tsx
+++ src/pages/StylePage/StylePage.tsx
@@ -26,173 +26,175 @@ import { MyForm } from "./Test";
 import BtnFilters from "../../components/Filters/BtnFilters";
 
 export const MainPage = () => {
-  const btnData = [
-    { name: "Все" },
-    { name: "Активные" },
-    { name: "Просмотренные" },
-    { name: "Черновик" },
-  ];
+    const btnData = [
+        { name: "Все" },
+        { name: "Активные" },
+        { name: "Просмотренные" },
+        { name: "Черновик" },
+    ];
 
-  const sort = [{ name: "По дате" }, { name: "По типу" }];
+    const sort = [{ name: "По дате" }, { name: "По типу" }];
 
-  const formConfig: FormikConfig<any> = {
-    initialValues: {
-      email: "",
-      password: "",
-    },
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  };
+    const formConfig: FormikConfig<any> = {
+        initialValues: {
+            email: "",
+            password: "",
+        },
+        onSubmit: (values) => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    };
 
-  const schemaConfig: Yup.ObjectShape = {
-    email: Yup.string()
-      .email("Некорректная почта!")
-      .required("Обязательное поле!"),
-    password: Yup.string()
-      .min(4, "Минимум 4 символа!")
-      .max(12, "Максимум 12 символов!")
-      .required("Обязательное поле!"),
-  };
+    const schemaConfig: Yup.ObjectShape = {
+        email: Yup.string()
+            .email("Некорректная почта!")
+            .required("Обязательное поле!"),
+        password: Yup.string()
+            .min(4, "Минимум 4 символа!")
+            .max(12, "Максимум 12 символов!")
+            .required("Обязательное поле!"),
+    };
 
-  return (
-    <div>
-      <div style={{ display: "flex" }}>
-        <div style={{ marginRight: 40, width: "40%" }}>
-          <h1>{`<h1>`} - Заголовок 1</h1>
-          <h2>{`<h2>`} - Заголовок 2</h2>
-          <h3>{`<h3>`} - Заголовок 3</h3>
-          <p>
-            {`<p>`} - Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Consectetur sunt ea cumque esse deleniti eligendi consequatur quae
-            tempore veniam possimus quasi aliquid aliquam, magnam fugiat,
-            ducimus voluptas eveniet minima deserunt.
-          </p>
-          <p className="min">{`<p>.min`} - микро текст</p>
-          <BtnGroupSelect view="radio" data={btnData} />
-          <div style={{ margin: "10px 0" }} />
-          <BtnGroupSelect view="select" data={btnData} />
-          <BtnFilters sort={sort} date={""}/>
-          <div style={{ margin: "10px 0" }} />
-          <div style={{ display: "flex" }}>
-            <div style={{ marginRight: 45 }}>
-              <p className="bold">{`<p>.bold`}</p>
-              <p className="medium">{`<p>.medium`}</p>
-              <p className="regular">{`<p>.regular`}</p>
-            </div>
-            <div>
-              <p className="bold-italic">{`<p>.bold-italic`}</p>
-              <p className="medium-italic">{`<p>.medium-italic`}</p>
-              <p className="regular-italic">{`<p>.regular-italic`}</p>
-            </div>
-          </div>
-        </div>
-        <div style={{ width: "60%" }}>
-          <div
-            style={{
-              display: "flex",
-              gap: "10px",
-              marginBottom: 10,
-            }}
-          >
-            <Widget info="4" icon={icon1} description="экспоната" />
-            <Widget info="от 250р." icon={icon2} description="платный вход" />
-            <Widget info="6+" icon={icon3} description="возраст" />
-            <Widget icon={icon7} info=" " description="23-26 июля" />
-            <Widget icon={icon4} description="Открытый вход" />
-          </div>
-          <div
-            style={{
-              display: "flex",
-              gap: "10px",
-              marginBottom: 10,
-            }}
-          >
-            <PublicationStatus status="Опубликовано" type="completed" />
-            <PublicationStatus status="Черновик" type="draft" />
-            <PublicationStatus status="Выставка" type="event" />
-          </div>
-          <StyledCard
-            eventTitle="Экспозиция музея"
-            dateOfCreation="22.02.2023"
-          />
-        </div>
-      </div>
-      <div>
-        <div style={{ margin: "10px 0" }} />
-        <DragAndDrop />
-        <div style={{ margin: "10px 0" }} />
-        <ImagesGallery />
-        <div style={{ margin: "10px 0" }} />
-      </div>
-      <div>
-        <FillForm />
-      </div>
-      <div style={{ marginTop: "10px", marginBottom: "10px" }}>
-        <FormContainer schemaConfig={schemaConfig} formConfig={formConfig}>
-          {(formik) => (
-            <div>
-              <div>
-                <div style={{ display: "flex", gap: "10px" }}>
-                  <ButtonArt icon={registerIcon} type="submit">
-                    Зарегистрироваться
-                  </ButtonArt>
-                  <ButtonArt
-                    icon={loginIcon}
-                    iconWidth={25}
-                    style={{
-                      backgroundColor: "#282828",
-                      border: "2px solid #282828",
-                    }}
-                  >
-                    Выйти
-                  </ButtonArt>
-                </div>
-
-                <FormInput name="email" label="Email" />
-                <FormInput
-                  name="password"
-                  label="Пароль"
-                  help="От 4 до 12 символов"
-                />
-              </div>
-            </div>
-          )}
-        </FormContainer>
-      </div>
-      <MyForm />
-      <div style={{ display: "flex", gap: "10px" }}>
-        <div style={{ width: "300px" }}>
-          <InfoComponent
-            icon={icon9}
-            title={"Не может быть опубликовано"}
-            desc={"Есть незаполненные поля"}
-          />
-        </div>
-        <div style={{ width: "300px" }}>
-          <InfoComponent
-            icon={icon8}
-            title={"Успешно опубликовано"}
-            desc={"Ошибок не найдено"}
-          />
-        </div>
+    return (
         <div>
-          <InfoComponent
-            iconWidth={100}
-            icon={icon7}
-            title={"А тут здоровая иконка"}
-            desc={"Прикол да"}
-          />
+            <div style={{ display: "flex" }}>
+                <div style={{ marginRight: 40, width: "40%" }}>
+                    <h1>{`<h1>`} - Заголовок 1</h1>
+                    <h2>{`<h2>`} - Заголовок 2</h2>
+                    <h3>{`<h3>`} - Заголовок 3</h3>
+                    <p>
+                        {`<p>`} - Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Consectetur sunt ea cumque esse deleniti eligendi consequatur quae
+                        tempore veniam possimus quasi aliquid aliquam, magnam fugiat,
+                        ducimus voluptas eveniet minima deserunt.
+                    </p>
+                    <p className="min">{`<p>.min`} - микро текст</p>
+                    <BtnGroupSelect view="radio" data={btnData} />
+                    <div style={{ margin: "10px 0" }} />
+                    <BtnGroupSelect view="select" data={btnData} />
+                    <BtnFilters sort={sort} date={""} />
+                    <div style={{ margin: "10px 0" }} />
+                    <div style={{ display: "flex" }}>
+                        <div style={{ marginRight: 45 }}>
+                            <p className="bold">{`<p>.bold`}</p>
+                            <p className="medium">{`<p>.medium`}</p>
+                            <p className="regular">{`<p>.regular`}</p>
+                        </div>
+                        <div>
+                            <p className="bold-italic">{`<p>.bold-italic`}</p>
+                            <p className="medium-italic">{`<p>.medium-italic`}</p>
+                            <p className="regular-italic">{`<p>.regular-italic`}</p>
+                        </div>
+                    </div>
+                </div>
+                <div style={{ width: "60%" }}>
+                    <div
+                        style={{
+                            display: "flex",
+                            gap: "10px",
+                            marginBottom: 10,
+                        }}
+                    >
+                        <Widget info="4" icon={icon1} description="экспоната" />
+                        <Widget info="от 250р." icon={icon2} description="платный вход" />
+                        <Widget info="6+" icon={icon3} description="возраст" />
+                        <Widget icon={icon7} info=" " description="23-26 июля" />
+                        <Widget icon={icon4} description="Открытый вход" />
+                    </div>
+                    <div
+                        style={{
+                            display: "flex",
+                            gap: "10px",
+                            marginBottom: 10,
+                        }}
+                    >
+                        <PublicationStatus status="Опубликовано" type="completed" />
+                        <PublicationStatus status="Черновик" type="draft" />
+                        <PublicationStatus status="Выставка" type="event" />
+                    </div>
+                    <StyledCard
+                        eventTitle="Экспозиция музея"
+                        dateOfCreation="22.02.2023"
+                        type='Выставка'
+                        status="completed"
+                    />
+                </div>
+            </div>
+            <div>
+                <div style={{ margin: "10px 0" }} />
+                <DragAndDrop />
+                <div style={{ margin: "10px 0" }} />
+                <ImagesGallery />
+                <div style={{ margin: "10px 0" }} />
+            </div>
+            <div>
+                <FillForm />
+            </div>
+            <div style={{ marginTop: "10px", marginBottom: "10px" }}>
+                <FormContainer schemaConfig={schemaConfig} formConfig={formConfig}>
+                    {(formik) => (
+                        <div>
+                            <div>
+                                <div style={{ display: "flex", gap: "10px" }}>
+                                    <ButtonArt icon={registerIcon} type="submit">
+                                        Зарегистрироваться
+                                    </ButtonArt>
+                                    <ButtonArt
+                                        icon={loginIcon}
+                                        iconWidth={25}
+                                        style={{
+                                            backgroundColor: "#282828",
+                                            border: "2px solid #282828",
+                                        }}
+                                    >
+                                        Выйти
+                                    </ButtonArt>
+                                </div>
+
+                                <FormInput name="email" label="Email" />
+                                <FormInput
+                                    name="password"
+                                    label="Пароль"
+                                    help="От 4 до 12 символов"
+                                />
+                            </div>
+                        </div>
+                    )}
+                </FormContainer>
+            </div>
+            <MyForm />
+            <div style={{ display: "flex", gap: "10px" }}>
+                <div style={{ width: "300px" }}>
+                    <InfoComponent
+                        icon={icon9}
+                        title={"Не может быть опубликовано"}
+                        desc={"Есть незаполненные поля"}
+                    />
+                </div>
+                <div style={{ width: "300px" }}>
+                    <InfoComponent
+                        icon={icon8}
+                        title={"Успешно опубликовано"}
+                        desc={"Ошибок не найдено"}
+                    />
+                </div>
+                <div>
+                    <InfoComponent
+                        iconWidth={100}
+                        icon={icon7}
+                        title={"А тут здоровая иконка"}
+                        desc={"Прикол да"}
+                    />
+                </div>
+            </div>
+            <InfoComponent
+                iconWidth={50}
+                icon={icon4}
+                title={"Пример адаптивности"}
+                desc={
+                    "Лорем ипсум долор сит амет, цонсецтетуер адиписцинг елит. Аенеан вулпутате маурис ид аугуе, алияуам тинцидунт нулла ац, пеллентескуе сед маурис. Нам а цонгуе еуисмод елеифенд. Нулла рисус орнаре етим, егестас вел лигула. Сед егестас фелис а дуи, моллис ут рисус ат, моллис моллис рисус."
+                }
+            />
         </div>
-      </div>
-      <InfoComponent
-        iconWidth={50}
-        icon={icon4}
-        title={"Пример адаптивности"}
-        desc={
-          "Лорем ипсум долор сит амет, цонсецтетуер адиписцинг елит. Аенеан вулпутате маурис ид аугуе, алияуам тинцидунт нулла ац, пеллентескуе сед маурис. Нам а цонгуе еуисмод елеифенд. Нулла рисус орнаре етим, егестас вел лигула. Сед егестас фелис а дуи, моллис ут рисус ат, моллис моллис рисус."
-        }
-      />
-    </div>
-  );
+    );
 };
