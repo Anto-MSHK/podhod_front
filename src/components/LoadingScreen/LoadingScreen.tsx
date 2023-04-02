b5d7e9e2acea7022b9f964659431a48f8b62105a
Rain <silverain1qw@gmail.com>
2023-04-02 12:44:07 +0300

    Pull Request reject fix

--- src/components/LoadingScreen/LoadingScreen.tsx
+++ src/components/LoadingScreen/LoadingScreen.tsx
@@ -1,6 +1,6 @@
-import React, {FC, useEffect, useState} from "react";
+import React, { FC, useEffect, useState } from "react";
 import { Spinner } from "reactstrap";
-import styles from './LoadingScreen.module.css'
+import styles from "./LoadingScreen.module.css";
 
 interface LoadingScreenI {
     isLoading: boolean;
@@ -8,6 +8,7 @@ interface LoadingScreenI {
 
 export const LoadingScreen: FC<LoadingScreenI> = ({ isLoading }) => {
     const [mounted, setMounted] = useState(true);
+    const [spinnerCount, setSpinnerCount] = useState(165);
 
     useEffect(() => {
         return () => {
@@ -16,21 +17,49 @@ export const LoadingScreen: FC<LoadingScreenI> = ({ isLoading }) => {
     }, []);
 
     useEffect(() => {
-        if (isLoading && mounted) {
-            document.body.style.overflow = "hidden";
-        } else {
+        const updateSpinnerCount = () => {
+            const width = window.innerWidth;
+            const height = window.innerHeight;
+            const spinnersPerRow = Math.ceil(width / 100);
+            const numberOfRows = Math.ceil(height / 100);
+            setSpinnerCount(spinnersPerRow * numberOfRows);
+        };
+
+        window.addEventListener("resize", updateSpinnerCount);
+        updateSpinnerCount();
+
+        return () => {
+            window.removeEventListener("resize", updateSpinnerCount);
+        };
+    }, []);
+
+    useEffect(() => {
+        const handleClassChange = () => {
+            if (isLoading) {
+                document.body.classList.add("no-scrollbar");
+                document.body.style.overflow = "hidden";
+            } else {
+                document.body.classList.remove("no-scrollbar");
+                document.body.style.overflow = "auto";
+            }
+        };
+
+        handleClassChange();
+
+        return () => {
+            document.body.classList.remove("no-scrollbar");
             document.body.style.overflow = "auto";
-        }
-    }, [isLoading, mounted]);
+        };
+    }, [isLoading]);
+
 
     return (
-        <div className={styles.loading_screen}>
-            {[...Array(165)].map((_,index) => (
-                <Spinner key={index}
-                         type="grow"
-                         className={styles.spinner}
-                />
-            ))}
-        </div>
+        <>
+                <div className={styles.loading_screen}>
+                    {[...Array(spinnerCount)].map((_, index) => (
+                        <Spinner key={index} type="grow" className={styles.spinner} />
+                    ))}
+                </div>
+        </>
     );
 };
