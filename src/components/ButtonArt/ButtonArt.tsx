623ac544024fbd3e40003c9f60facd99fa6a4aa4
DULCETq <maks.lebedev0404@mail.ru>
2023-04-19 13:42:38 +0300

    some fixes

--- src/components/ButtonArt/ButtonArt.tsx
+++ src/components/ButtonArt/ButtonArt.tsx
@@ -12,32 +12,25 @@ interface ButtonProps {
     icon?: string;
 }
 
-export const ButtonArt = ({size, variant, onClick, text, icon}: ButtonProps) => {
+export const ButtonArt = ({ size, variant, onClick, text, icon }: ButtonProps) => {
     const buttonClassName = `${styles.button} ${styles[size]} ${styles[variant]}`;
 
     const renderArrow = () => (
-        <>
-            <span className={styles.text}></span>
-            <div className={styles.arrowContainer}>
-                <span className={styles.arrow}>
-                <div className={styles.arrowLine}/>
-                <img src={Arrow} alt="arrow"/>
-            </span>
-            </div>
-        </>
-    );
-
-    const renderTextAndIcon = () => (
-        <span className={styles.textAndIcon}>
-          {text && <span className={styles.text}>{text}</span>}
-            <img src={icon} alt="icon"/>
-        </span>
+        <div className={styles.arrowContainer}>
+      <span className={styles.arrow}>
+        <div className={styles.arrowLine} />
+        <img src={Arrow} alt="arrow" />
+      </span>
+        </div>
     );
 
     return (
         <button className={buttonClassName} onClick={onClick}>
             {variant === 'arrow' && renderArrow()}
-            {variant === 'icon' || variant === 'textAndIcon' ? renderTextAndIcon() : text}
+            <span className={styles.textAndIcon}>
+                {text && <span className={styles.text}>{text}</span>}
+                {icon && <img src={icon} alt="icon"/>}
+            </span>
         </button>
     );
 };
