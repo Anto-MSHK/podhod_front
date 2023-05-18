fb69095b7b1b28d2e23eac4110cf57108a444c01
Anto-MSHK <anjiilop25@gmail.com>
2023-05-18 14:07:06 +0300

    fix all bugs; Anton krut

--- src/app/Slices/isChapterShownSlice.ts
+++ src/app/Slices/isChapterShownSlice.ts
@@ -1,17 +1,26 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export type isChapterShownStateT = {
-	[key: string]: boolean
-}
+	[key: string]: boolean;
+};
 
 const initialState: isChapterShownStateT = {};
 const isChapterShownSlice = createSlice({
-	name: 'isChapterShown',
+	name: "isChapterShown",
 	initialState,
 	reducers: {
-		toggleChapter: (state, action: PayloadAction<number>) => {
-			const chapterId = action.payload;
-			state[chapterId] = !state[chapterId];
+		toggleChapter: (state, action: PayloadAction<number | undefined>) => {
+			if (!action.payload) {
+				for (const [key, value] of Object.entries(state)) {
+					state[key] = false;
+				}
+			} else {
+				const chapterId = action.payload;
+				for (const [key, value] of Object.entries(state)) {
+					if (+chapterId !== +key) state[key] = false;
+				}
+				state[chapterId] = !state[chapterId];
+			}
 		},
 	},
 });
